b3346b9f46f0d8a047c03a2cc0bdff1cfde56416
Emma <emmajanefearn@aol.com>
2015-02-12 21:59:10 +0000

    All Menu Buttons Now Redirect to Pages
    Fully functional menu

--- app/src/main/java/emmajanefearn/financingapplication/Menu.java
+++ app/src/main/java/emmajanefearn/financingapplication/Menu.java
@@ -3,7 +3,6 @@ package emmajanefearn.financingapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,49 +15,53 @@ public class Menu extends ActionBarActivity {
 
         //Declare Buttons
         Button button = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+        Button button5 = (Button) findViewById(R.id.button5);
         Button button6 = (Button) findViewById(R.id.button6);
+
 
         //On click of a buttons listener
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("TAG", String.valueOf(v.getId()));
-                Log.d("TAG", String.valueOf(R.id.button));
                 switch (v.getId()) {
                     case R.id.button:
-                        Log.d("TAG", "Button 1");
                         Intent intent = new Intent(Menu.this, Overview.class);
                         startActivity(intent);
                         break;
-                    case R.id.button6:
-                        Log.d("TAG", "Button 2");
-                        Intent intent2 = new Intent(Menu.this, LogExpenditure.class);
+                    case R.id.button2:
+                        Intent intent2 = new Intent(Menu.this, Recommendations.class);
                         startActivity(intent2);
+                        break;
+                    case R.id.button3:
+                        Intent intent3 = new Intent(Menu.this, Outgoings.class);
+                        startActivity(intent3);
+                        break;
+                    case R.id.button4:
+                        Intent intent4 = new Intent(Menu.this, Income.class);
+                        startActivity(intent4);
+                        break;
+                    case R.id.button5:
+                        Intent intent5 = new Intent(Menu.this, Settings.class);
+                        startActivity(intent5);
+                        break;
+                    case R.id.button6:
+                        Intent intent6 = new Intent(Menu.this, LogExpenditure.class);
+                        startActivity(intent6);
                         break;
                 }
             }
         };
 
-        //Set their listener for some reason idk
+        //Set the listener
         button.setOnClickListener(listener);
+        button2.setOnClickListener(listener);
+        button3.setOnClickListener(listener);
+        button4.setOnClickListener(listener);
+        button5.setOnClickListener(listener);
         button6.setOnClickListener(listener);
     }
-       /* public void onClick(View v){
-            Log.d("TAG", String.valueOf(v.getId()));
-            Log.d("TAG", String.valueOf(R.id.button));
-            switch (v.getId()) {
-                case R.id.button:
-                    Log.d("TAG", "test");
-                    Intent intent = new Intent(Menu.this, Overview.class);
-                    Log.d("TAG", "hello");
-                    startActivity(intent);
-                    Log.d("TAG", "world");
-                    break;
-                case R.id.button6:
-                    Intent intent2 = new Intent(Menu.this, LogExpenditure.class);
-                    startActivity(intent2);
-                    break;
-                }
-    }*/
 }
 
